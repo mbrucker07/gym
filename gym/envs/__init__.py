af8b0b6a47ea9a0cf7f2ae5d62cb2654f1e75e87
Matthias Brucker <matthias.brucker@hotmail.de>
2020-01-22 17:43:42 +0100

    config

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -423,28 +423,28 @@ for reward_type in ['sparse', 'dense']:
         id='FetchPushObstacle{}-v1'.format(suffix),
         entry_point='gym.envs.robotics:FetchPushObstacleEnv',
         kwargs=kwargs,
-        max_episode_steps=50,
+        max_episode_steps=100,
     )
 
     register(
         id='FetchPushNoObstacle{}-v1'.format(suffix),
         entry_point='gym.envs.robotics:FetchPushNoObstacleEnv',
         kwargs=kwargs,
-        max_episode_steps=50,
+        max_episode_steps=100,
     )
 
     register(
         id='FetchPickObstacle{}-v1'.format(suffix),
         entry_point='gym.envs.robotics:FetchPickObstacleEnv',
         kwargs=kwargs,
-        max_episode_steps=50,
+        max_episode_steps=100,
     )
 
     register(
         id='FetchPickNoObstacle{}-v1'.format(suffix),
         entry_point='gym.envs.robotics:FetchPickNoObstacleEnv',
         kwargs=kwargs,
-        max_episode_steps=50,
+        max_episode_steps=100,
     )
 
     register(
