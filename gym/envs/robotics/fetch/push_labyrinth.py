a02f387ee68a5526b23adc5cc2f657e0b867947c
Matthias Brucker <matthias.brucker@hotmail.de>
2020-02-06 14:28:39 +0100

    fix

--- gym/envs/robotics/fetch/push_labyrinth.py
+++ gym/envs/robotics/fetch/push_labyrinth.py
@@ -56,7 +56,7 @@ class FetchPushLabyrinthEnv(robot_env.RobotEnv, gym.utils.EzPickle):
         self.adapt_dict["obstacles"] = [[1.3 - 0.1, 0.75, 0.6 - 0.1, 0.11, 0.02, 0.1],
                                         [1.3 - 0.23, 0.75, 0.6 - 0.1, 0.02, 0.35, 0.1],
                                         [1.3 + 0.03, 0.75, 0.6 - 0.1, 0.02, 0.2, 0.1]]
-        self.adapt_dict["spaces"] = [20, 20, 10] # [30, 30, 10]
+        self.adapt_dict["spaces"] = [30, 30, 10] # [30, 30, 10]
         self.adapt_dict["z_penalty"] = 10# [30, 30, 10]
 
         super(FetchPushLabyrinthEnv, self).__init__(
