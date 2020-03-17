f3e1c3131b3207d6c76626933a61b9129b03feb3
Matthias Brucker <matthias.brucker@hotmail.de>
2020-03-17 11:18:43 +0100

    z-penalty fix: made field smaller

--- gym/envs/robotics/fetch/push_labyrinth.py
+++ gym/envs/robotics/fetch/push_labyrinth.py
@@ -52,12 +52,12 @@ class FetchPushLabyrinthEnv(robot_env.RobotEnv, gym.utils.EzPickle):
 
         # TODO: configure adaption parameters
         self.adapt_dict=dict()
-        self.adapt_dict["field"] = [1.3, 0.75, 0.6, 0.25, 0.35, 0.2]
-        self.adapt_dict["obstacles"] = [[1.3 - 0.1, 0.75, 0.6 - 0.1, 0.11, 0.02, 0.1],
-                                        [1.3 - 0.23, 0.75, 0.6 - 0.1, 0.02, 0.35, 0.1],
-                                        [1.3 + 0.03, 0.75, 0.6 - 0.1, 0.02, 0.2, 0.1]]
+        self.adapt_dict["field"] = [1.3, 0.75, 0.5, 0.25, 0.35, 0.1]
+        self.adapt_dict["obstacles"] = [[1.3 - 0.1, 0.75, 0.5, 0.11, 0.02, 0.1],
+                                        [1.3 - 0.23, 0.75, 0.5, 0.02, 0.35, 0.1],
+                                        [1.3 + 0.03, 0.75, 0.5, 0.02, 0.2, 0.1]]
         self.adapt_dict["spaces"] = [30, 30, 10] # [30, 30, 10]
-        self.adapt_dict["z_penalty"] = 10# [30, 30, 10]
+        self.adapt_dict["z_penalty"] = 1
 
         super(FetchPushLabyrinthEnv, self).__init__(
             model_path=model_path, n_substeps=n_substeps, n_actions=4,
